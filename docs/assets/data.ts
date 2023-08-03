1934320c9aecbf575c9188a83323d62c1ef35486
Bairon Developer <baironf.bernalg@ecci.edu.co>
2023-08-02 23:00:23 -0500

    deploy

---
+++ docs/assets/data.ts
@@ -0,0 +1,103 @@
+export const data = {
+    califications: [{
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },
+    {
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },
+    {
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },
+    {
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },
+    {
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },
+    {
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },
+    {
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },
+    {
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },
+    {
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },
+    {
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },
+    {
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },
+    {
+        url_image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+        name: 'LAURA SMITH',
+        comment: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube'
+    },],
+    courses: [{
+            id: 1,
+            name: 'Course 1',
+            description: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube',
+            buttonName: 'READ MORE'
+        },
+        {
+            id: 2,
+            name: 'Course 2',
+            description: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube',
+            buttonName: 'READ MORE'
+        },
+        {
+            id: 3,
+            name: 'Course 3',
+            description: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube',
+            buttonName: 'READ MORE'
+        }, {
+            id: 4,
+            name: 'Course 4',
+            description: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube',
+            buttonName: 'READ MORE'
+        }
+
+    ],
+    us: [{
+        title: 'About',
+        title_purple: 'US.',
+        img: 'assets/images/us/1.png',
+        description: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube',
+        button_name: 'READ MORE',
+        items: ['Automatically detect','Automatically detect', 'Automatically detect', 'Automatically detect']
+    },
+    {
+        title: 'Why',
+        title_purple: 'US.',
+        img: 'assets/images/us/2.png',
+        description: 'Automatically detect Security Vulnerabilities and Security Hotspots during your code review. Write the most efficient code possible with SonarQube',
+        button_name: 'READ MORE',
+        items: ['Automatically detect','Automatically detect', 'Automatically detect', 'Automatically detect']
+    },]
+}
